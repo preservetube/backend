5f401f05889ff20346533230c220fe46fd52b16f
unknown <89595418+unknownsrc@users.noreply.github.com>
2023-03-03 19:17:39 +0100

    going back to hcaptcha

--- utils/captcha.js
+++ utils/captcha.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 async function checkCaptcha(response) {
-    const confirm = await (await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+    const confirm = await (await fetch('https://hcaptcha.com/siteverify', {
         method: 'POST',
         body: new URLSearchParams({
             'response': response,
