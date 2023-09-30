8bdc0738f9da149f565b4d3e9fe63bd4f23cc170
localhost <o0l2qp3e@duck.com>
2023-09-30 17:32:32 +0200

    dompurify ytdlp messages

--- utils/ytdlp.js
+++ utils/ytdlp.js
@@ -1,4 +1,5 @@
 const child_process = require('child_process')
+const DOMPurify = require('isomorphic-dompurify')
 
 async function downloadVideo(url, ws) {
     return new Promise((resolve, reject) => {
@@ -8,7 +9,14 @@ async function downloadVideo(url, ws) {
             const msg = data.toString().trim()
             if (!msg) return 
     
-            if (ws) ws.send(`DATA - ${msg}`)
+            if (ws) ws.send(`DATA - ${DOMPurify.sanitize(msg)}`)
+        })
+
+        child.stderr.on("data", data => {
+            const msg = data.toString().trim()
+            if (!msg) return 
+    
+            if (ws) ws.send(`DATA - ${DOMPurify.sanitize(msg)}`)
         })
 
         child.on("close", async (code, signal) => {
