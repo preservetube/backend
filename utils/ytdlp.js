5b70a7265cf2107bf34c8e11071b88b17fe80392
localhost <o0l2qp3e@duck.com>
2023-10-04 16:27:20 +0200

    gluetun proxy

--- utils/ytdlp.js
+++ utils/ytdlp.js
@@ -3,7 +3,7 @@ const DOMPurify = require('isomorphic-dompurify')
 
 async function downloadVideo(url, ws) {
     return new Promise((resolve, reject) => {
-        const child = child_process.spawn("../yt-dlp", ['--proxy http://gluetun:8888', url], {cwd: 'videos', shell: false})
+        const child = child_process.spawn("../yt-dlp", ["--proxy", "http://gluetun:8888", url], {cwd: 'videos', shell: false})
         // https://github.com/yt-dlp/yt-dlp/blob/cc8d8441524ec3442d7c0d3f8f33f15b66aa06f3/README.md?plain=1#L1500
         
         child.stdout.on("data", data => {
