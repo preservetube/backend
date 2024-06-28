bbe9844828608ee2e30b16f9bdbbbc02c3f6284e
localhost <o0l2qp3e@duck.com>
2024-06-28 20:02:14 +0200

    http instead of https

--- utils/ytdlp.js
+++ utils/ytdlp.js
@@ -30,7 +30,7 @@ async function downloadVideo(url, ws, id) {
         const alreadyPrecentages = []
 
         const target = Array.isArray(downloadJson.url) ? downloadJson.url[0] : downloadJson.url
-        const download = wget.download(target, `./videos/${id}.mp4`)
+        const download = wget.download(target.replace('https://', 'http://'), `./videos/${id}.mp4`)
         
         download.on('start', fileSize => {
             size = fileSize
