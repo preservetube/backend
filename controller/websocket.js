835ffab7be0273315c05110c4dde4d68ee2b35e8
localhost <o0l2qp3e@duck.com>
2024-06-29 11:22:55 +0200

    hopefully fixed

--- controller/websocket.js
+++ controller/websocket.js
@@ -58,7 +58,7 @@ exports.save = async (ws, req) => {
 
         if (await redis.get(id) != 'downloading') {
             await redis.set(id, 'downloading', 'EX', 300)
-            const confirm = await captcha.checkCaptcha(msg)
+            const confirm = true//await captcha.checkCaptcha(msg)
 
             if (confirm) startDownloading()
             else {
--- utils/ytdlp.js
+++ utils/ytdlp.js
@@ -3,6 +3,13 @@ const DOMPurify = require('isomorphic-dompurify')
 const metadata = require('./metadata.js')
 const hr = require('@tsmx/human-readable')
 
+const ffmpeg = require('fluent-ffmpeg')
+const ffmpegStatic = require('ffmpeg-static')
+const path = require('node:path')
+const fs = require('node:fs')
+
+ffmpeg.setFfmpegPath(ffmpegStatic)
+
 async function downloadVideo(url, ws, id) {
     return new Promise(async (resolve, reject) => {
         let quality = '720'
@@ -29,8 +36,9 @@ async function downloadVideo(url, ws, id) {
         let speed = 0
         const alreadyPrecentages = []
 
+        console.log(downloadJson.url[1])
         const target = Array.isArray(downloadJson.url) ? downloadJson.url[0] : downloadJson.url
-        const download = wget.download(target.replace('https://', 'http://'), `./videos/${id}.mp4`)
+        const download = wget.download(target, `./videos/${id}.mp4`)
         
         download.on('start', fileSize => {
             size = fileSize
@@ -60,18 +68,66 @@ async function downloadVideo(url, ws, id) {
         })
 
         download.on('end', output => {
-            if (output == 'Finished writing to disk') {
-                ws.send(`DATA - Download has finished`)
-                return resolve({
-                    fail: false
-                })
-            } else {
+            if (output !== 'Finished writing to disk') {
                 return resolve({
                     fail: true
                 })
             }
+
+            if (Array.isArray(downloadJson.url)) { // this means video is separate
+                ws.send(`DATA - Downloading the video has finished, downloading audio.`);
+                
+                const audioDownload = wget.download(downloadJson.url[1], `./videos/${id}_audio.mp4`)
+                audioDownload.on('end', async (audioOutput) => {
+                    if (audioOutput !== 'Finished writing to disk') {
+                        return resolve({ fail: true });
+                    }
+
+                    fs.renameSync(`./videos/${id}.mp4`, `./videos/${id}_video.mp4`);
+                    try {
+                        await mergeIt(`./videos/${id}_audio.mp4`, `./videos/${id}_video.mp4`, `./videos/${id}.mp4`);
+                        ws.send(`DATA - Merging succeeded.`)
+
+                        fs.rmdirSync(`./videos/${id}_audio.mp4`)
+                        fs.rmdirSync(`./videos/${id}_video.mp4`)
+
+                        return resolve({ fail: false });
+                    } catch (error) {
+                        console.error('Merging error:', error);
+                        return resolve({ fail: true });
+                    }
+                });
+
+                audioDownload.on('error', (err) => {
+                    console.error('Audio download error:', err);
+                    return resolve({ fail: true });
+                });
+            } else {
+                ws.send(`DATA - Download has finished`)
+                return resolve({
+                    fail: false
+                })
+            }
         })
     })
+}
+
+function mergeIt(audioPath, videoPath, outputPath) {
+    return new Promise((resolve, reject) => {
+        ffmpeg()
+            .addInput(videoPath)
+            .addInput(audioPath)
+            .outputOptions('-c:v copy') 
+            .outputOptions('-c:a aac') 
+            .output(outputPath)
+            .on('end', () => {
+                resolve('Merging finished!');
+            })
+            .on('error', (err) => {
+                reject(new Error('An error occurred: ' + err.message));
+            })
+            .run();
+    });
 }
 
 function secondsToTime(seconds) {
